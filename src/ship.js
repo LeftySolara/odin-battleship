e9dd9e2440f6ace000ccb92e048b96c4a70c46d4
Jalen Adams <jalen@jalenkadams.me>
2020-11-24 17:13:17 -0500

    Add: more tests for ship factory

--- src/ship.js
+++ src/ship.js
@@ -7,19 +7,35 @@ const SHIP_LENGTHS = Object.freeze({
 });
 
 const createShip = (type) => {
+  if (!(type in SHIP_LENGTHS)) {
+    return null;
+  }
+
   const length = SHIP_LENGTHS[type];
   const hits = Array(length);
   hits.fill(false);
   Object.seal(hits);
 
   function hit(location) {
+    if (typeof location !== 'number' || location < 0 || location >= length) {
+      return;
+    }
     hits[location] = true;
+  }
+
+  function isSunk() {
+    return hits.indexOf(false) === -1;
   }
 
   const getLength = () => length;
   const getHits = () => hits;
 
-  return { hit, getLength, getHits };
+  return {
+    hit,
+    isSunk,
+    getLength,
+    getHits,
+  };
 };
 
 export default createShip;
--- test/ship.test.js
+++ test/ship.test.js
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals';
 import createShip from '../src/ship';
 
 describe('Ship factory', () => {
-  test('Different ships have different lengths', () => {
+  test('Different types of ships can be created', () => {
     const carrier = createShip('carrier');
     expect(carrier.getLength()).toBe(5);
 
@@ -17,13 +17,56 @@ describe('Ship factory', () => {
 
     const destroyer = createShip('destroyer');
     expect(destroyer.getLength()).toBe(1);
+
+    const nullShip = createShip('this ship does not exist');
+    expect(nullShip).toStrictEqual(null);
+
+    const blankShip = createShip();
+    expect(blankShip).toStrictEqual(null);
   });
 
   test('Ships can be hit', () => {
     const carrier = createShip('carrier');
+
     carrier.hit(1);
+    expect(carrier.getHits()).toStrictEqual([false, true, false, false, false]);
+
     carrier.hit(3);
-    const hits = carrier.getHits();
-    expect(hits).toStrictEqual([false, true, false, true, false]);
+    expect(carrier.getHits()).toStrictEqual([false, true, false, true, false]);
+  });
+  test('Trying to hit an already-hit spot does nothing', () => {
+    const battleship = createShip('battleship');
+
+    battleship.hit(2);
+    expect(battleship.getHits()).toStrictEqual([false, false, true, false]);
+
+    battleship.hit(2);
+    expect(battleship.getHits()).toStrictEqual([false, false, true, false]);
+  });
+  test('Trying to hit a spot outside the ship does nothing', () => {
+    const battleship = createShip('battleship');
+    expect(battleship.getHits()).toStrictEqual([false, false, false, false]);
+
+    battleship.hit(-1);
+    expect(battleship.getHits()).toStrictEqual([false, false, false, false]);
+
+    battleship.hit(100);
+    expect(battleship.getHits()).toStrictEqual([false, false, false, false]);
+
+    battleship.hit('1');
+    expect(battleship.getHits()).toStrictEqual([false, false, false, false]);
+
+    battleship.hit();
+    expect(battleship.getHits()).toStrictEqual([false, false, false, false]);
+  });
+  test('A ship can be sunk', () => {
+    const cruiser = createShip('cruiser');
+    expect(cruiser.isSunk()).toBe(false);
+    cruiser.hit(0);
+    expect(cruiser.isSunk()).toBe(false);
+    cruiser.hit(1);
+    expect(cruiser.isSunk()).toBe(false);
+    cruiser.hit(2);
+    expect(cruiser.isSunk()).toBe(true);
   });
 });
