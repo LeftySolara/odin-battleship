9ebd9d1df86a8d1674c08e8ae87069d7ff422f91
Jalen Adams <jalen@jalenkadams.me>
2020-11-24 21:23:46 -0500

    Document: add jsdoc comments for ship functions

--- src/ship.js
+++ src/ship.js
@@ -9,6 +9,12 @@ const SHIP_LENGTHS = Object.freeze({
   carrier: 5,
 });
 
+/**
+ * Factory function for creating ships.
+ *
+ * @param {string} type The type of ship to create.
+ * @returns A ship object.
+ */
 const createShip = (type) => {
   if (!(type in SHIP_LENGTHS)) {
     return null;
@@ -19,6 +25,14 @@ const createShip = (type) => {
   hits.fill(false);
   Object.seal(hits);
 
+  const getLength = () => length;
+  const getHits = () => hits;
+
+  /**
+   * Marks a location on the ship as being hit.
+   *
+   * @param {number} location The position of the ship to hit.
+   */
   function hit(location) {
     if (typeof location !== 'number' || location < 0 || location >= length) {
       return;
@@ -26,12 +40,13 @@ const createShip = (type) => {
     hits[location] = true;
   }
 
+  /**
+   * Checks whether the ship has been sunk.
+   * A ship is sunk when all its locations have been hit.
+   */
   function isSunk() {
     return hits.indexOf(false) === -1;
   }
-
-  const getLength = () => length;
-  const getHits = () => hits;
 
   return { hit, isSunk, getLength, getHits };
 };
